e803fb8446f5d914b564f450c5ada740f6d98ec7
Shawn@Centos <307882919@qq.com>
2018-08-25 10:28:31 +0800

    update 8/25

--- 5_3(类的静态成员)/5_3(静态数据&函数成员).cpp
+++ 5_3(类的静态成员)/5_3(静态数据&函数成员).cpp
@@ -5,10 +5,10 @@ using namespace std;
 
 class Point{
 public:
-	Point(int x = 0, int y = 0) : x(x), y(y){
+	Point(int x = 0, int y = 0) : x(x), y(y){	// 构造函数
 		count++;
 	}
-	Point(Point &p){
+	Point(Point &p){	// 复制构造函数
 		x = p.x;
 		y = p.y;
 		count++;
@@ -17,18 +17,18 @@ public:
 	int getX(){return x;}
 	int getY(){return y;}
 
-	static void showCount(){	// 
+	static void showCount(){	// 静态函数成员
 		cout << "Object count=" << count << endl;
 	}
 private:
 	int x, y;
-	static int count;	// 
+	static int count;	// 静态数据成员声明
 };
 
-int Point::count = 0;
+int Point::count = 0;	// 静态数据成员定义和初始化，使用类名限定
 
 int main(){
-	Point a(4, 5);
+	Point a(4, 5);	// 定义对象a，其构造函数会使count增1
 	cout << "Point A: " << a.getX() << ", " << a.getY();
 	Point::showCount();
 

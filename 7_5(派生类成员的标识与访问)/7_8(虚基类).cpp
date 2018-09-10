c9987b59124395693760518b3b170691b7296e94
Shawn@Centos <307882919@qq.com>
2018-09-10 22:48:39 +0800

    update 9/10

--- 7_5(派生类成员的标识与访问)/7_8(虚基类).cpp
+++ 7_5(派生类成员的标识与访问)/7_8(虚基类).cpp
@@ -4,22 +4,26 @@ using namespace std;
 
 class Base0{
 public:
+	// Base0(int var): var0(var){}
 	int var0;
 	void fun0(){cout<<"Member of Base0, var0="<<var0<<endl;}
 };
 
 class Base1: virtual public Base0{
 public:
+	// Base1(int var): Base0(var){}
 	int var1;
 };
 
 class Base2: virtual public Base0{
 public:
+	// Base2(int var): Base0(var){}
 	int var2;
 };
 
 class Derived: public Base1, public Base2{
 public:
+	// Derived(int var): Base0(var), Base1(var), Base2(var){}
 	int var;
 	void fun(){cout<<"Member of Derived"<<endl;}
 };

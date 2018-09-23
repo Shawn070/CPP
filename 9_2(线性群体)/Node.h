51216f2aef61df554262caa69b49112d38e76b8f
Shawn <307882919@qq.com>
2018-09-23 18:12:59 +0800

    add 9/23

--- 9_2(线性群体)/Node.h
+++ 9_2(线性群体)/Node.h
@@ -1,4 +1,5 @@
-//Node.h
+// 9_6.h
+// Node.h
 // 节点类模板
 
 #ifndef NODE_H
@@ -11,7 +12,7 @@ public:
     T data;                                 // 数据域
 
     Node (const T &data, Node<T> * next=0); // 构造函数
-    void insertAfter();                     // 在本结点之后插入一个同类节点P
+    void insertAfter(Node<T> * p);          // 在本结点之后插入一个同类节点P
     Node<T> * deleteAfter();                // 删除本结点的后继结点，并返回其地址
     Node<T> * nextNode();                   // 获取后继结点的地址
     const Node<T> * nextNode() const;       // 获取后继结点的地址
@@ -24,7 +25,31 @@ Node<T>::Node(const T& date, Node<T> * next /* =0*/): date(data), next(next){}
 
 // 返回后继结点的指针
 template<class T>
-Node<T>::Node(const T& data, Node<T> )
+Node<T> * Node<T>::nextNode(){
+    return next;
+}
 
+// 返回后继结点的指针
+template<class T>
+const Node<T> * Node<T>::nextNode() const{
+    return next;
+}
+
+// 在当前结点之后插入一个结点P
+template<class T>
+void Node<T>::insertAfter(Node<T> *p){
+    p->next=next;
+    next=p;
+}
+
+// 删除当前结点的后继结点，并返回其地址
+template<class T>
+Node<T> * Node<T>::deleteAfter(){
+    Node<T> * tempPtr=next;
+    if(next==0)
+        return 0;
+    next=tempPtr->next;
+    return tempPtr;
+}
 
 #endif

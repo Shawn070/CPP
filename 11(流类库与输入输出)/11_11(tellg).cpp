6b617f81f32155ca3a658cc6c6f01bcc7b437976
Shawn@Centos <307882919@qq.com>
2018-10-20 23:42:23 +0800

    add 10/20

---
+++ 11(流类库与输入输出)/11_11(tellg).cpp
@@ -0,0 +1,25 @@
+// 11_11.cpp
+// 读一个文件并显示出其中0元素的位置。
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main(){
+    ifstream file("integers", ios_base::in|ios_base::binary);
+    if(file){
+        while(file){
+            streampos here=file.tellg();
+            int v;
+            file.read(reinterpret_cast<char *>(&v), sizeof(int));
+            if(file && v==0)
+                cout<<"Position"<<here<<" is 0"<<endl;
+        }
+    }else{
+        cout<<"ERROR: Cannot open file 'integers'. "<<endl;
+    }
+    file.close();
+    return 0;
+}
+
+
+

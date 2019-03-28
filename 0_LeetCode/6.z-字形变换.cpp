979e591d0bdbb0e75389ab9b9aad514b3e06bc92
Shawn <307882919@qq.com>
2019-03-28 20:03:16 +0800

    add #6

--- 0_LeetCode/6.z-字形变换.cpp
+++ 0_LeetCode/6.z-字形变换.cpp
@@ -47,7 +47,19 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-        
+        if(numRows == 1) return s;
+        string ret;
+        int n = s.size();
+        int cycleLen = 2 * numRows - 2;
+        for(int i=0; i < numRows; i++){
+            for(int j=0; i + j < n; j += cycleLen){
+                ret += s[i + j];
+                if(i != 0 && i != numRows - 1 && j + cycleLen - i < n){
+                    ret += s[j + cycleLen - i];
+                }
+            }
+        }
+        return ret;
     }
 };
 
